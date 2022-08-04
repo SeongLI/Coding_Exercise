d62b3bdb71eac4b19790163f70f8f976fc142af2
SeongLI <pro30343069@gmail.com>
2022-08-04 15:51:29 +0900

    first commit

---
+++ Shortest_Path/전보.py
@@ -0,0 +1,57 @@
+# N개의 도시가 있다. 도시 C에서 출발하여 각 도시 사이에 설치된 통로를 거쳐, 최대한 많이 퍼져나가려 한다.
+# 각 도시의 번호와 통로가 설치되어 있는 정보가 주어졌을 때, 도시 C에서 보낸 메시지를 받게 되는 도시의 개수는 총 몇 개이며
+# 도시들이 모두 메시지를 받는 데 까지 걸리는 시간은 얼마인지 계산하는 프로그램을 작성하라.
+
+# 출력 조건 : 첫째 줄에 도시 C에서 보낸 메시지를 받는 도시의 총 개수와 총 걸리는 시간을 공백으로 구분하여 출력한다.
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+def dijkstra(start) :
+    q = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q :   # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist :
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now] :
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]] :
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+# 노드의 개수, 간선은 개수, 시작 노드를 입력받기
+n, m, start = map(int, input().split())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    #  X번 노드에서 Y번 노드로 가는 비용이 Z라는 의미
+    graph[x].append((y,z))
+
+# 다익스트라 알고리즘을 수행
+dijkstra(start)
+
+# 도달할 수 있는 노드의 개수
+count = 0
+# 도달할 수 있는 노드 중에서, 가장 멀리 있는 노드와의 최단 거리
+max_distance = 0
+for d in distance :
+    # 도달할 수 있는 노드인 경우
+    if d != 1e9:
+        count += 1
+        max_distance = max(max_distance, d)
+
+# 시작 노드는 제외해야 하므로, count -1 을 출력
+print(count -1 , max_distance)
